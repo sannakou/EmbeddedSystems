fe7804bb6c745574d09cf035a59e4b865fc68433
sannakou <s.koukkula@hotmail.com>
2025-09-15 13:19:55 +0300

    toisto lisätty sekvensseihin

--- Week3_RTOS_part2/trafficlights_sequence/src/uart_dispatcher.c
+++ Week3_RTOS_part2/trafficlights_sequence/src/uart_dispatcher.c
@@ -1,12 +1,14 @@
 #include <zephyr/kernel.h>
 #include <zephyr/sys/printk.h>
 #include <zephyr/drivers/uart.h>
+#include <stdlib.h>
+#include <string.h>
 #include "leds.h"
 #include "buttons.h"
-#include <string.h>
 
 #define STACKSIZE 700
 #define PRIORITY  5
+#define UART_MSG_MAX_LEN 64
 
 #define UART_DEVICE_NODE DT_CHOSEN(zephyr_shell_uart)
 static const struct device *const uart_dev = DEVICE_DT_GET(UART_DEVICE_NODE);
@@ -14,7 +16,7 @@ static const struct device *const uart_dev = DEVICE_DT_GET(UART_DEVICE_NODE);
 /* Dispatcher FIFO data */
 struct data_t {
     void *fifo_reserved;
-    char msg;
+    char msg[UART_MSG_MAX_LEN];
 };
 
 /* Dispatcher FIFO */
@@ -32,16 +34,31 @@ int init_uart(void) {
 /* UART task */
 static void uart_task(void *a, void *b, void *c) {
     char rc;
+    char uart_buf[UART_MSG_MAX_LEN];
+    int idx = 0;
+    memset(uart_buf, 0, sizeof(uart_buf));
+
     while (1) {
         if (uart_poll_in(uart_dev, &rc) == 0) {
-            if (rc == 'R' || rc == 'G' || rc == 'Y') {
-                struct data_t *buf = k_malloc(sizeof(*buf));
-                if (buf) {
-                    buf->msg = rc;
-                    k_fifo_put(get_button_fifo(), buf);
-                    k_sem_give(&dispatcher_sem);
-                    printk("UART msg queued: %c\n", buf->msg);
+            if (rc == '\r' || rc == '\n') {
+                if (idx > 0) {
+                    struct data_t *buf = k_malloc(sizeof(*buf));
+                    if (buf) {
+                        strncpy(buf->msg, uart_buf, sizeof(buf->msg) - 1);
+                        buf->msg[sizeof(buf->msg) - 1] = '\0';
+                        k_fifo_put(get_button_fifo(), buf);
+                        k_sem_give(&dispatcher_sem);
+                        printk("UART seq queued: %s\n", buf->msg);
+                    }
+                    idx = 0;
+                    memset(uart_buf, 0, sizeof(uart_buf));
                 }
+            } else if (idx < (UART_MSG_MAX_LEN - 1)) {
+                uart_buf[idx++] = rc;
+            } else {
+                printk("UART message too long, dropped\n");
+                idx = 0;
+                memset(uart_buf, 0, sizeof(uart_buf));
             }
         } else {
             k_msleep(5);
@@ -56,28 +73,43 @@ static void dispatcher_task(void *a, void *b, void *c) {
 
         struct data_t *rec;
         while ((rec = k_fifo_get(get_button_fifo(), K_NO_WAIT)) != NULL) {
-            switch (rec->msg) {
-            case 'R':
-                gpio_pin_set_dt(&red, 1);
-                k_msleep(500);
-                gpio_pin_set_dt(&red, 0);
-                break;
-            case 'G':
-                gpio_pin_set_dt(&green, 1);
-                k_msleep(500);
-                gpio_pin_set_dt(&green, 0);
-                break;
-            case 'Y':
-                gpio_pin_set_dt(&red, 1);
-                gpio_pin_set_dt(&green, 1);
-                k_msleep(500);
-                gpio_pin_set_dt(&red, 0);
-                gpio_pin_set_dt(&green, 0);
-                break;
-            default:
-                printk("Unknown character: %c\n", rec->msg);
-                break;
+            char *sep = strchr(rec->msg, ',');
+            int repeat = 1;
+
+            if (sep != NULL) {
+                *sep = '\0'; // erotellaan sekvenssi ja toisto
+                repeat = atoi(sep + 1);
+                if (repeat <= 0) repeat = 1;
             }
+
+            for (int r = 0; r < repeat; r++) {
+                for (size_t i = 0; i < strlen(rec->msg); i++) {
+                    char ch = rec->msg[i];
+                    switch (ch) {
+                    case 'R':
+                        gpio_pin_set_dt(&red, 1);
+                        k_msleep(500);
+                        gpio_pin_set_dt(&red, 0);
+                        break;
+                    case 'G':
+                        gpio_pin_set_dt(&green, 1);
+                        k_msleep(500);
+                        gpio_pin_set_dt(&green, 0);
+                        break;
+                    case 'Y':
+                        gpio_pin_set_dt(&red, 1);
+                        gpio_pin_set_dt(&green, 1);
+                        k_msleep(500);
+                        gpio_pin_set_dt(&red, 0);
+                        gpio_pin_set_dt(&green, 0);
+                        break;
+                    default:
+                        printk("Unknown character: %c\n", ch);
+                        break;
+                    }
+                }
+            }
+
             k_free(rec);
         }
     }
