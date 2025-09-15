2943e22090eb8fab804f3e4c2b177745e63cd8fc
sannakou <s.koukkula@hotmail.com>
2025-09-15 13:24:59 +0300

    kommentit lisätty mainin alkuun

--- Week3_RTOS_part2/trafficlights_sequence/src/main.c
+++ Week3_RTOS_part2/trafficlights_sequence/src/main.c
@@ -1,3 +1,9 @@
+/* 
+Tavoitteena korkein pistemäärä, eli koodissa toimii 
+1. sekvenssien lähetys sarjaportin kautta 
+2. sekvessien lähetys painonapeilla
+3. sekvenssien toisto lisäämällä sekvenssin perään "," ja toistojen määrä
+*/
 #include <zephyr/kernel.h>
 #include <zephyr/sys/printk.h>
 #include "leds.h"
